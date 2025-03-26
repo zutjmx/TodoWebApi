9d47c38a9a367f1a67fd7c9c4981e2abe0f124d4
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-03-25 23:01:10 -0600

    first commit

---
+++ TodoAPI/Program.cs
@@ -0,0 +1,40 @@
+// program.cs
+using TodoAPI.AppDataContext;
+using TodoAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// Add  This to in the Program.cs file
+builder.Services.Configure<DbSettings>(builder.Configuration.GetSection("DbSettings")); // Add this line
+builder.Services.AddSingleton<TodoDbContext>(); // Add this line
+
+
+var app = builder.Build();
+
+// Add this line
+{
+    using var scope = app.Services.CreateScope(); // Add this line
+    var context = scope.ServiceProvider; // Add this line
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
